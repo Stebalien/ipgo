b709b4e8197b716528d8a15798bff639dbf1a35b
Steven Allen <steven@stebalien.com>
2021-09-24 11:59:19 +0100

    feat: rewrite
    ipgo can now completely replace the go command, automatically mirroring
    all dependencies into IPFS.

---
+++ go.mod
@@ -0,0 +1,39 @@
+module github.com/Stebalien/ipgo
+
+go 1.17
+
+require (
+	github.com/gofrs/flock v0.8.1
+	github.com/ipfs/go-ipfs-api v0.2.0
+	github.com/ipfs/go-ipfs-files v0.0.8
+	github.com/multiformats/go-multibase v0.0.3
+	github.com/multiformats/go-multihash v0.0.16
+	go.uber.org/multierr v1.7.0
+)
+
+require (
+	github.com/btcsuite/btcd v0.20.1-beta // indirect
+	github.com/crackcomm/go-gitignore v0.0.0-20170627025303-887ab5e44cc3 // indirect
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/ipfs/go-cid v0.0.7 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.4 // indirect
+	github.com/libp2p/go-buffer-pool v0.0.2 // indirect
+	github.com/libp2p/go-flow-metrics v0.0.3 // indirect
+	github.com/libp2p/go-libp2p-core v0.6.1 // indirect
+	github.com/libp2p/go-openssl v0.0.7 // indirect
+	github.com/minio/blake2b-simd v0.0.0-20160723061019-3f5f724cb5b1 // indirect
+	github.com/minio/sha256-simd v1.0.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
+	github.com/multiformats/go-base32 v0.0.3 // indirect
+	github.com/multiformats/go-base36 v0.1.0 // indirect
+	github.com/multiformats/go-multiaddr v0.3.0 // indirect
+	github.com/multiformats/go-multiaddr-net v0.2.0 // indirect
+	github.com/multiformats/go-varint v0.0.6 // indirect
+	github.com/spacemonkeygo/spacelog v0.0.0-20180420211403-2296661a0572 // indirect
+	github.com/whyrusleeping/tar-utils v0.0.0-20180509141711-8c6c8ba81d5c // indirect
+	go.opencensus.io v0.22.4 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83 // indirect
+	golang.org/x/sys v0.0.0-20210309074719-68d13333faf2 // indirect
+)
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
@@ -12,20 +13,46 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"path/filepath"
 	"strings"
+	"sync"
 	"unicode"
 
-	"github.com/ipfs/go-ipfs-api"
-	"github.com/ipfs/go-ipfs-files"
+	"github.com/gofrs/flock"
+	shell "github.com/ipfs/go-ipfs-api"
+	files "github.com/ipfs/go-ipfs-files"
+	"github.com/multiformats/go-multibase"
+	"github.com/multiformats/go-multihash"
+	"go.uber.org/multierr"
 )
 
 var (
-	GoModCache    = path.Join(build.Default.GOPATH, "pkg", "mod", "cache")
-	GoModDownload = path.Join(GoModCache, "download")
+	GoProxy       = "direct"
+	GoModCache    = filepath.Join(build.Default.GOPATH, "pkg", "mod", "cache")
+	GoModDownload = filepath.Join(GoModCache, "download")
 	Gateway       = "http://127.0.0.1:8080"
-	DepsFile      = "deps.ipfs"
+	DepsFile      = "go.ipfs"
 	GoCommand     = "go"
 )
+
+func init() {
+	res, err := exec.Command(GoCommand, "env", "GOMODCACHE", "GOPROXY").Output()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: failed to read go env: %s\n", err)
+		return
+	}
+	lines := strings.Split(string(res), "\n")
+	if len(lines) != 2 {
+		return
+	}
+	if lines[0] != "" {
+		GoModCache = lines[0]
+		GoModDownload = filepath.Join(GoModCache, "download")
+	}
+	if lines[1] != "" {
+		GoProxy = lines[1]
+	}
+}
 
 func escapePath(s string) string {
 	var builder strings.Builder
@@ -54,22 +81,21 @@ type download struct {
 	Error string
 }
 
-func getArtifacts() (results []artifact, err error) {
-	pkgs, err := getPackages()
+func (m *IpfsMod) getArtifacts() (results []artifact, err error) {
+	pkgs, err := m.getPackages()
 	if err != nil {
 		return nil, err
 	}
 	// TODO: Remove this. See https://github.com/golang/go/issues/29772
-	meta, err := getMetadata()
+	meta, err := m.getMetadata()
 	if err != nil {
 		return nil, err
 	}
 	return append(pkgs, meta...), nil
 }
 
-func getPackages() (results []artifact, err error) {
-	cmd := exec.Command(GoCommand, "mod", "download", "-json")
-	cmd.Env = append(os.Environ(), "GO111MODULE=on")
+func (m *IpfsMod) getPackages() (results []artifact, err error) {
+	cmd := m.Command("mod", "download", "-json")
 	cmd.Stderr = os.Stderr
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
@@ -105,9 +131,8 @@ func getPackages() (results []artifact, err error) {
 	}
 }
 
-func getMetadata() (results []artifact, err error) {
-	cmd := exec.Command(GoCommand, "mod", "graph")
-	cmd.Env = append(os.Environ(), "GO111MODULE=on")
+func (m *IpfsMod) getMetadata() (results []artifact, err error) {
+	cmd := m.Command("mod", "graph")
 	cmd.Stderr = os.Stderr
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
@@ -143,7 +168,6 @@ func getMetadata() (results []artifact, err error) {
 		result.Version = dep[verIdx+1:]
 		basePath := path.Join(GoModDownload, escapePath(result.Path), "@v", result.Version)
 		result.GoMod = basePath + ".mod"
-		result.Info = basePath + ".info"
 		results = append(results, result)
 	}
 	return results, lineScanner.Err()
@@ -214,6 +238,36 @@ func newFileSet() fileSet {
 	return fileSet{files: make(map[string]interface{})}
 }
 
+type lazyFileReader struct {
+	openOnce sync.Once
+	path     string
+
+	fi  *os.File
+	err error
+}
+
+func newLazyFileReader(path string) *lazyFileReader {
+	return &lazyFileReader{path: path}
+}
+
+func (l *lazyFileReader) Read(buf []byte) (int, error) {
+	l.openOnce.Do(func() {
+		l.fi, l.err = os.Open(l.path)
+	})
+	if l.err != nil {
+		return 0, l.err
+	}
+	return l.fi.Read(buf)
+}
+
+func (l *lazyFileReader) Close() error {
+	l.openOnce.Do(func() {})
+	if l.fi != nil {
+		return l.fi.Close()
+	}
+	return l.err
+}
+
 func artifactsToDirectory(artifacts []artifact) (files.Directory, error) {
 	dir := newFileSet()
 	added := make(map[string]struct{})
@@ -228,12 +282,7 @@ func artifactsToDirectory(artifacts []artifact) (files.Directory, error) {
 
 			added[p] = struct{}{}
 
-			file, err := os.Open(p)
-			if err != nil {
-				dir.Close()
-				return nil, err
-			}
-			if !dir.add(relativeArtifact(p), file) {
+			if !dir.add(relativeArtifact(p), newLazyFileReader(p)) {
 				dir.Close()
 				return nil, fmt.Errorf("BUG: file conflict at %q", p)
 			}
@@ -288,39 +337,125 @@ func ipfsAdd(s *shell.Shell, dir files.Directory) (string, error) {
 	return final, nil
 }
 
-func runGo(args ...string) error {
+type IpfsMod struct {
+	Path string `json:"path"`
+	Hash string `json:"checksum"`
+}
+
+func Load() (*IpfsMod, error) {
+	// Default to "empty".
+	var lock IpfsMod
+
 	depPathBytes, err := ioutil.ReadFile(DepsFile)
+	if os.IsNotExist(err) {
+		return &lock, nil
+	} else if err != nil {
+		return nil, fmt.Errorf("failed to read %q: %s", DepsFile, err)
+	} else if err := json.Unmarshal(depPathBytes, &lock); err != nil {
+		return nil, fmt.Errorf("failed to parse %q: %s", DepsFile, err)
+	}
+	return &lock, nil
+}
+
+func (m *IpfsMod) Command(args ...string) *exec.Cmd {
 	cmd := exec.Command(GoCommand, args...)
+	if m.Path != "" {
+		cmd.Env = append(os.Environ(),
+			"GO111MODULE=on",
+			"GOPROXY="+Gateway+m.Path+","+GoProxy,
+		)
+	}
+	return cmd
+}
+
+func (m *IpfsMod) Run(args ...string) error {
+	cmd := m.Command(args...)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
-	if err == nil {
-		depPath := strings.Trim(string(depPathBytes), " \n\r\t")
-		cmd.Env = append(os.Environ(), "GO111MODULE=on", "GOPROXY="+Gateway+string(depPath))
-	} else if os.IsNotExist(err) {
-		// skip.
-	} else {
-		return fmt.Errorf("failed to read 'ipfs.deps': %s", err)
-	}
 	return cmd.Run()
 }
 
-// IpfsImport imports the package's dependencies into IPFS.
-func IpfsImport() (string, error) {
+func (m *IpfsMod) goSumFile() (*os.File, error) {
+	modInfo, err := exec.Command(GoCommand, "list", "-m", "-json").Output()
+	if err != nil {
+		return nil, err
+	}
+	var info struct{ Dir string }
+	if err := json.Unmarshal(modInfo, &info); err != nil {
+		return nil, err
+	}
+	if info.Dir == "" {
+		return nil, nil
+	}
+	f, err := os.Open(filepath.Join(info.Dir, "go.sum"))
+	if os.IsNotExist(err) {
+		err = nil
+	}
+	return f, err
+}
+
+func (m *IpfsMod) checkHash(file io.Reader) (bool, error) {
+	if m.Hash == "" {
+		return false, nil
+	}
+
+	_, decoded, err := multibase.Decode(m.Hash)
+	if err != nil {
+		return false, err
+	}
+	info, err := multihash.Decode(decoded)
+	if err != nil {
+		return false, err
+	}
+
+	if mh, err := multihash.SumStream(file, info.Code, info.Length); err != nil {
+		return false, err
+	} else {
+		return bytes.Equal(mh, decoded), nil
+	}
+}
+
+func (m *IpfsMod) Update() error {
+	// Check the gosum file.
+	goSumFile, err := m.goSumFile()
+	if err != nil {
+		return err
+	} else if goSumFile == nil {
+		// If it doesn't exist, there's nothing to update.
+		return nil
+	}
+
+	defer goSumFile.Close()
+
+	lock := flock.New(DepsFile)
+
+	// Now take the write lock so we don't conflict with other updates.
+	lock.Lock()
+	defer lock.Unlock()
+
+	// If the hash is the same, there's nothing to do.
+	same, err := m.checkHash(goSumFile)
+	if same || err != nil {
+		return err
+	}
+
+	// Ok, something has changed.
+
 	s := shell.NewLocalShell()
 	if s == nil {
 		// TODO: Public instance?
-		return "", fmt.Errorf("can't connect to local IPFS instance")
+		return fmt.Errorf("can't connect to local IPFS instance")
 	}
 
-	artifacts, err := getArtifacts()
+	artifacts, err := m.getArtifacts()
 	if err != nil {
-		return "", err
+		return err
 	}
 
 	dir, err := artifactsToDirectory(artifacts)
 	if err != nil {
-		return "", err
+		return err
 	}
 
 	finalHash, err := ipfsAdd(
@@ -330,33 +465,39 @@ func IpfsImport() (string, error) {
 		}),
 	)
 	if err != nil {
-		return "", err
+		return err
 	}
-	depPath := "/ipfs/" + finalHash
-	return depPath, nil
-}
 
-// UpdateDepsFile is a imports the deps into IPFS and then writes out the deps
-// file.
-func UpdateDepsFile() (string, error) {
-	depPath, err := IpfsImport()
-	if err != nil {
-		return "", err
+	// Re-hash the sum file as it may have changed.
+	if _, err := goSumFile.Seek(0, os.SEEK_SET); err != nil {
+		return err
 	}
-	return depPath, ioutil.WriteFile(DepsFile, []byte(depPath), 0644)
+	if hash, err := multihash.SumStream(goSumFile, multihash.SHA2_256, -1); err != nil {
+		return err
+	} else if hash, err := multibase.Encode(multibase.Base32, hash); err != nil {
+		return err
+	} else {
+		m.Hash = hash
+	}
+	m.Path = "/ipfs/" + finalHash
+
+	if fi, err := os.Create(DepsFile); err != nil {
+		return err
+	} else if err := json.NewEncoder(fi).Encode(m); err != nil {
+		_ = fi.Close()
+		return err
+	} else {
+		return fi.Close()
+	}
 }
 
 func run() error {
 	args := os.Args[1:]
-	if len(args) == 2 && args[0] == "mod" && args[1] == "ipfs-lock" {
-		depPath, err := UpdateDepsFile()
-		if err != nil {
-			return err
-		}
-		fmt.Printf("dependencies published to: %s\n", depPath)
-		return nil
+	mod, err := Load()
+	if err != nil {
+		return err
 	}
-	return runGo(args...)
+	return multierr.Combine(mod.Run(args...), mod.Update())
 }
 
 func main() {
